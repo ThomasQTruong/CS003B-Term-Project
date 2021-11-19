166b3e084802e00b0f1745a230949fcb02beb9c0
Thomas Truong <TruongThomasQ@gmail.com>
2021-11-19 13:55:34 -0800

    Created TicTacToeV2Util::setAmountToWin(int winAmount).

--- src/TicTacToeV2Util.java
+++ src/TicTacToeV2Util.java
@@ -11,9 +11,30 @@ public class TicTacToeV2Util {
   /**
    * Constructor; configures amountToWin.
    *
-   * @param winAmount - the amount to win.
+   * @param winAmount - the amount of marks needed in a row to win.
    */
   public TicTacToeV2Util(int winAmount) {
+    // Win amount needs to be >= 3 and <= player + 1.
+    if (winAmount < 3) {
+      throw new IllegalArgumentException(
+          "TicTacToeV2Util(int winAmount): winAmount cannot be less than 3.");
+    }
+    if (winAmount > Player.size() + 1) {
+      throw new IllegalArgumentException(
+          "TicTacToeV2Util(int winAmount): winAmount cannot be greater than [Player.size() + 1].");
+    }
+
+    // Valid winAmount given.
+    amountToWin = winAmount;
+  }
+
+
+  /**
+   * Sets the value of amountToWin to a different number.
+   *
+   * @param winAmount - the amount of marks needed in a row to win.
+   */
+  public void setAmountToWin(int winAmount) {
     // Win amount needs to be >= 3 and <= player + 1.
     if (winAmount < 3) {
       throw new IllegalArgumentException(
